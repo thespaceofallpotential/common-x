9ae6783f56cfb1d9deafd49a0c1fc4348f4fa3fa
aeim <anexerciseinmental@outlook.com>
2025-08-29 18:04:35 +0100

    aeim

--- src/core/SymmetricIndex.ts
+++ src/core/SymmetricIndex.ts
@@ -6,13 +6,19 @@ import { Position, Token, TypeArray, TypeSet, Word } from "./Types.ts";
  * > Map: a map is a lower-dimensional representation of arbitrarily-plural 
  * higher-dimensional form
  * 
- * A Symmetric-Index maps higher-dimensional form to index-keys which represent 
- * (that) indexed-form literally
+ * A Symmetric-Index maps higher-dimensional form to index-keys, which represent 
+ * indexed-form "literally"
  * 
  * In this way, a Symmetric-Index (or a map generally) represents and exploits 
  * symmetries-of-form (between arbitrarily-different phenomena/dimensional-spaces) 
  * to enable interrogation and navigation of higher-dimensions, at the 
  * (correspondingly lower) cost of lower-dimensional key-space (/representation-space)
+ * 
+ * > for example: in reality, we can use a map to learn about an area, and 
+ * > how to get somewhere, without needing to physically move; similarly, we can use
+ * > (smaller) lower-dimensional abstract spaces to avoid the costs of (larger)
+ * > higher-dimensional ones
+ * 
  */
 export type SymmetricIndex<T extends Token | Word> = Map<T, Array<Position>>;
 
