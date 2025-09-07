8275a9be7e8de5ce29c9725eadaf9cbac910fba0
causalmechanics <94231817+causalmechanics@users.noreply.github.com>
2025-09-07 10:07:00 +0100

    Update generation_1_runner.py

--- src/generation_1_runner.py
+++ src/generation_1_runner.py
@@ -7,17 +7,24 @@ sequences = source.get_sequences()
 
 a, b = sequences
 
+# all solvers ./gen_1/*
+
 # brute-force solver
+# > basic naieve
 run_and_print(a, b, ProcessorTypes.BRUTE_FORCE)
 
 # constituent solver
+# > structure-less approximation
 run_and_print(a, b, ProcessorTypes.CONSTITUENT)
 
-# cultivted solver: search-free "grown" solution; growth from curated parts/ cultivated environment
+# cultivted solver
+# > search-free "grown" solution; growth from curated parts/ cultivated environment
 run_and_print(a, b, ProcessorTypes.CULTIVATED)
 
-# deductive resolver: knowledge-free "whittled" solution; iterative elimination of negative-space
+# deductive resolver
+# > knowledge-free "whittled" solution; iterative elimination of negative-space
 run_and_print(a, b, ProcessorTypes.DEDUCTIVE)
 
-# positive projecttion  solver
+# positive projection solver
+# > partial optimisation, to illustrate "hallucination space"
 run_and_print(a, b, ProcessorTypes.POSITIVE_PROJECTION)
